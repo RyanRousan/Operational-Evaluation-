efe7504de09fb7079b104eb9f4f538d40ff7ca34
RyanRousan <hayan.rousan@gmail.com>
2025-04-16 19:21:58 -0500

    Create index.js

---
+++ src/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import OperationalEvaluationQuiz from "./OperationalEvaluationQuiz";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <OperationalEvaluationQuiz />
+  </React.StrictMode>
+);
